bbfed183242aec89f9491a0a87083fc177cedc69
ClaPascarella <claudiopascarella1996@gmail.com>
2024-05-03 18:31:24 +0200

    genero 5 numeri casulai

---
+++ main.js
@@ -0,0 +1,17 @@
+let = random()
+
+function random () {
+
+    for (let i = 0; i < 5; i++) {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        console.log(randomNumber);
+    }
+    
+    
+}
+
+
+
+
+
+
